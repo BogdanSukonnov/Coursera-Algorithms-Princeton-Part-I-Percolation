ca392d24ea0929cb443b13d0b4689828127ff169
Bogdan Sukonnov <bogdan.sukonnov@gmail.com>
2020-03-14 22:47:39 +0300

    all done

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,9 +1,15 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: Bogdan Sukonnov
+ *  Date: 14.03.2020
+ *  Description: We model a percolation system using an n-by-n grid of sites.
+ * Each site is either open or blocked. A full site is an open site that can be
+ * connected to an open site in the top row via a chain of neighboring
+ * (left, right, up, down) open sites. We say the system percolates if there
+ * is a full site in the bottom row. In other words, a system percolates if we
+ * fill all open sites connected to the top row and that process fills some
+ * open site on the bottom row.
  **************************************************************************** */
 public class Percolation {
 
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,45 +1,73 @@
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: Bogdan Sukonnov
+ *  Date: 14.03.2020
+ *  Description: To perform a series of computational experiments
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private double[] results;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if (n <= 0) throw new IllegalArgumentException("grid size must be more than zero");
         if (trials <= 0) throw new IllegalArgumentException("trials number must be more than zero");
+        int nSquare = n * n;
+        results = new double[trials];
+        int[] indices = getIndices(nSquare);
+        for (int trial = 1; trial <= trials; ++trial) {
+            Percolation percolation = new Percolation(n);
+            StdRandom.shuffle(indices);
+            for (int index : indices) {
+                percolation.open(1 + index / n, 1 + index % n);
+                if (percolation.percolates()) {
+                    results[trial - 1] = (double) percolation.numberOfOpenSites() / nSquare;
+                    break;
+                }
+            }
+        }
+    }
+
+    private int[] getIndices(int nSquare) {
+        int[] indices = new int[nSquare];
+        for (int index = 0; index < nSquare; ++index) {
+            indices[index] = index;
+        }
+        return indices;
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return  0.0;
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0.0;
+        return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return 0.0;
+        return mean() - 1.96 * stddev() / Math.sqrt(results.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return 0.0;
+        return mean() + 1.96 * stddev() / Math.sqrt(results.length);
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        /** takes two command-line arguments n and T,
-         * performs T independent computational experiments (discussed above)
-         * on an n-by-n grid, and prints the sample mean, sample standard deviation,
-         * and the 95% confidence interval for the percolation threshold.
-         * Use StdRandom to generate random numbers;
-         * use StdStats to compute the sample mean and sample standard deviation.         *
-         */
+        if (args.length != 2) throw new IllegalArgumentException("waiting for 2 arguments");
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]),
+                                                      Integer.parseInt(args[1]));
+        StdOut.printf("%-23s = %s\n", "mean", stats.mean());
+        StdOut.printf("%-23s = %s\n", "stddev", stats.stddev());
+        StdOut.printf("%-23s = [%s, %s]\n", "95% confidence interval", stats.confidenceLo(),
+                      stats.confidenceHi());
     }
 }
